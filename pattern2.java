92601697dbc5f4df3817ae40f7a1bdf915371ad4
Apriandisyafiqfirdaus <friangamal@gmail.com>
2023-01-21 09:10:03 +0700

    file berjalan lancar

---
+++ pattern2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class pattern2 {
+
+  public static void main(String[] args) {
+    System.out.println("Enter a String: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        sc.close();
+        int length= s.length();
+        
+    char [] a=s.toCharArray();
+    
+    System.out.println("\nPrinting the pattern: ");
+    
+        for (int i = length-1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(a[j]);
+            }
+            System.out.println();
+        }
+
+  }
+}
